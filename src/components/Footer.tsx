4a6b25e5e999ea72b708b99cdd98bb1bff9e29a8
GeissonChagas <geisson.excel@gmail.com>
2023-03-01 14:36:40 -0300

    feat: adicionando componentes header/footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__content">
+        <p>© 2023 To Do. Todos os direitos reservados.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__title">
+        <h1>To Do</h1>
+        <h2>Gerencie suas tarefas de forma simples e prática</h2>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
