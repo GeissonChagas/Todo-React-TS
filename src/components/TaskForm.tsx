21c9ab33bc85e67629ed463680b1bd2a37d6b9a4
GeissonChagas <geisson.excel@gmail.com>
2023-03-04 00:05:33 -0300

    style: estilizando formulário

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+//Styles
+import styles from './TaskForm.module.css'
+
 interface Props{
     btnText: string
 }
@@ -7,12 +10,12 @@ interface Props{
 
 const TaskForm = ({btnText}: Props) => {
     return (
-        <form>
-            <div>
+        <form className={styles.form}>
+            <div className={styles.input_container}>
                 <label htmlFor="title">Título:</label>
                 <input type="text" name="title" placeholder="Digite o título da tarefa"/>
             </div>
-            <div>
+            <div className={styles.input_container}>
                 <label htmlFor="horario">Horário:</label>
                 <input type="time" name="horario" placeholder="Digite o horário da tarefa"/>
             </div>
